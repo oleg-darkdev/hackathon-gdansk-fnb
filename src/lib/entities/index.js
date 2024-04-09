dfabeb4987967f23187b6ecf518c31b1ad1d6d8b
DarkDev <darkdev_brand@riseup.net>
2024-04-09 19:35:09 +0300

    upd api

--- src/lib/entities/index.js
+++ src/lib/entities/index.js
@@ -2,5 +2,6 @@ import ProgectCard from './ui/landing/ProgectCard.svelte';
 import MagnetCard from './ui/landing/MagnetCard.svelte';
 import NewsCard from './ui/landing/NewsCard.svelte';
 import DarkDevCard from './ui/landing/DarkDevCard.svelte';
+import DonateBlock from './ui/landing/DonateBlock.svelte';
 
-export { ProgectCard, MagnetCard, NewsCard, DarkDevCard };
+export { ProgectCard, MagnetCard, NewsCard,  DonateBlock, DarkDevCard };
