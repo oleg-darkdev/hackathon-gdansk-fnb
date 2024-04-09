a6d6ef85018151a577ac87e356c81b404c7c162a
DarkDev <darkdev_brand@riseup.net>
2024-04-09 15:16:09 +0300

    stable v.

---
+++ src/lib/shared/ui/EmailLink.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let bg;
+</script>
+
+<a class=" social-item" href="mailto:fnbgdansk@zieloni.pl" target="_blank">
+	<img
+		src="./images/social/email.svg"
+		class="hover:delay-550 hover:-translate-y-1 hover:scale-105 hover:bg-white hover:bg-white hover:transition hover:duration-1000 hover:ease-in-out {bg} h-20 w-20"
+		alt=""
+	/>
+</a>
---
+++ src/lib/shared/ui/SocialLink.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let link, image, bg;
+</script>
+
+<a class=" social-item" href={link} target="_blank">
+	<img
+		src="./images/social/{image}"
+		class="hover:delay-550 hover:-translate-y-1 hover:scale-105 hover:bg-white hover:transition hover:duration-1000 hover:ease-in-out {bg} h-20 w-20"
+		alt=""
+	/>
+</a>
