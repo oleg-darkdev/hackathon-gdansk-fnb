341ca6167b1985c1bec19a74d285ca99450c1ce7
DarkDev <darkdev_brand@riseup.net>
2024-04-09 18:19:40 +0300

    add projects info

--- src/lib/shared/data/projects.js
+++ src/lib/shared/data/projects.js
@@ -1,27 +1,27 @@
 const projects = [
 	{
-		title: '',
-		desc: ['', ''],
+		title: 'Food Not Bombs official',
+		desc: 'Lista wszystkich oddziałów Food Not Bombs, które udostępniły swoje dane.',
+		link: 'https://foodnotbombs.net/info/locations/'
+	},
+	{
+		title: 'My gdanszczanie',
+		desc: 'Grupą społeczników niezaangażowanych w żadną z partii politycznych ani komitetów.',
+		link: 'https://www.mygdanszczanie.pl/'
+	},
+	{
+		title: '44Games',
+		desc: 'Libertarian educational boardgames',
 		link: '/'
 	},
 	{
-		title: '',
-		desc: ['', ''],
-		link: '/'
+		title: 'Anarchistyczny Czarny Krzyż',
+		desc: 'Międzynarodowa organizacja pomocy więźniom politycznym oraz osobom represjonowanym za działalność antyrządową, antyautorytarną, bądź polityczno-społeczną.',
+		link: 'https://www.ack.org.pl/'
 	},
-	{
+		{
 		title: '',
-		desc: ['', ''],
-		link: '/'
-	},
-	{
-		title: '',
-		desc: ['', ''],
-		link: '/'
-	},
-	{
-		title: '',
-		desc: ['', ''],
+		desc: '',
 		link: '/'
 	}
 ];
