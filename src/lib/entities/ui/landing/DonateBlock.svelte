6764a0f4ca47a473a75d4b884e59ad49e049bf28
DarkDev <darkdev_brand@riseup.net>
2024-04-09 19:36:51 +0300

    stable v.

---
+++ src/lib/entities/ui/landing/DonateBlock.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div class="hero w-full hero-default hero-primary svelte-1sbfvgs border">
+	<div class="hero-body">
+		<h2 class="hero-title svelte-1sbfvgs">UUUUUUUPS</h2>
+		<p class="hero-text-lead svelte-1sbfvgs">
+			Wygląda na to, że ta funkcja nie jest jeszcze dostępna - ale ponieważ ktoś kliknął przycisk,
+			dobrym pomysłem było zrobienie tego przycisku.
+		</p>
+		<div />
+	</div>
+</div>
---
+++ src/lib/shared/ui/DonateBtn.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let text, donateBtnPress;
+</script>
+
+<button
+	on:click={() => {
+		donateBtnPress = true;
+		console.log(donateBtnPress);
+	}}
+	class="big-btn background-primary text-primary hover:bg-neutral-200">{text} PLN</button
+>
+
+<style>
+	.big-btn {
+		display: block;
+		text-align: center;
+		text-transform: uppercase;
+		padding: 1rem;
+		cursor: pointer;
+		font-size: 2rem;
+		box-shadow: 0 0 0 6px #dec022;
+	}
+
+	@media (min-width: 550px) {
+		.big-btn {
+			padding: 1rem 2rem;
+		}
+	}
+</style>
