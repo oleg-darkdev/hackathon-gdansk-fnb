79ab7378d727c370b808b3cc2ce8b43b32ee3047
DarkDev <darkdev_brand@riseup.net>
2024-04-09 16:49:08 +0300

    add id`s

--- src/lib/shared/data/magnets.js
+++ src/lib/shared/data/magnets.js
@@ -5,7 +5,8 @@ const magnets = [
 		price: '3',
 		img: '01.png',
 		code: '',
-		link: '/'
+		link: '/',
+		id: 1
 	},
 	{
 		title: 'Drewniany magnes na lodówkę: OI!',
@@ -13,7 +14,8 @@ const magnets = [
 		price: '3',
 		img: '02.png',
 		code: '',
-		link: '/'
+		link: '/',
+		id: 2
 	},
 	{
 		title: 'Drewniany magnes na lodówkę: Girl Power',
@@ -21,7 +23,8 @@ const magnets = [
 		price: '3',
 		img: '03.png',
 		code: '',
-		link: '/'
+		link: '/',
+		id: 3
 	},
 	{
 		title: 'Drewniany magnes na lodówkę: Strong woman',
@@ -29,7 +32,62 @@ const magnets = [
 		price: '3',
 		img: '04.png',
 		code: '',
-		link: '/'
+		link: '/',
+		id: 4
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: OI!',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '02.png',
+		code: '',
+		link: '/',
+		id: 5
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: Girl Power',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '03.png',
+		code: '',
+		link: '/',
+		id: 6
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: Strong woman',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '04.png',
+		code: '',
+		link: '/',
+		id: 7
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: OI!',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '02.png',
+		code: '',
+		link: '/',
+		id: 8
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: Girl Power',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '03.png',
+		code: '',
+		link: '/',
+		id: 9
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: Strong woman',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '04.png',
+		code: '',
+		link: '/',
+		id: 10
 	}
 	// {
 	// 	title: 'Lorem ipsum',
