6527a0214578fb4c2fd9db139e39dd71c5d63456
DarkDev <darkdev_brand@riseup.net>
2024-04-09 15:16:37 +0300

    alfa v.

---
+++ src/lib/entities/ui/landing/ProgectCard.svelte
@@ -0,0 +1,16 @@
+<script>
+	import {} from 'spaper';
+
+	export let progectData;
+</script>
+
+<div class="item">
+	<div class="item__desc">
+		<div class="item__name">Hummus &amp; Tortilla Chips</div>
+		<div class="item__price">$5.5</div>
+		<div class="item__calories">333 calories</div>
+	</div>
+</div>
+
+<style>
+</style>
