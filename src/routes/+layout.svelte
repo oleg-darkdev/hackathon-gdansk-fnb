3105619ebd193439df14cc32785271c5d5828242
DarkDev <darkdev_brand@riseup.net>
2024-04-09 15:15:13 +0300

    refactoring, clear

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,41 @@
 <script lang="ts">
 	import '../app.postcss';
+	import 'papercss/dist/paper.min.css';
 
 	import { language } from '@inlang/sdk-js';
-	import {ThemeSwitcher, SvelteTheme, LocaleSwitcher} from '$shared/';
+	import { ThemeSwitcher, SvelteTheme, LocaleSwitcher } from '$shared/';
 
 	export let data;
 </script>
 
-	<!-- <a href="/{language}">
+<!-- <a href="/{language}">
 		<h1>Home</h1>
 	</a> -->
 
-	<LocaleSwitcher />
+<!-- <LocaleSwitcher />
 
-	<ThemeSwitcher />
+<ThemeSwitcher /> -->
 
-<slot />
+<div class="body">
+	<slot />
+</div>
 
-<SvelteTheme attribute="data-theme" />
+<!-- <SvelteTheme attribute="data-theme" /> -->
+
+<style>
+	:global(a) {
+		background-image: none;
+	}
 
 
-style
+	.body {
+		background: #e2dccc;
+		line-height: 1.2;
+		padding: 1rem;
+	}
+	@media (min-width: 880px) {
+		.body {
+			padding: 3rem;
+		}
+	}
+</style>
