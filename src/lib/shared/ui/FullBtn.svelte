ca5d56d19ad216f6e34bc64909291b1294d0a392
DarkDev <darkdev_brand@riseup.net>
2024-04-09 15:18:13 +0300

    alfa v.

---
+++ src/lib/shared/ui/FullBtn.svelte
@@ -0,0 +1,25 @@
+<a class="menu-download">Download the menu</a>
+
+<style>
+	.menu-download {
+		display: block;
+		background: #6f7bd0;
+		text-align: center;
+		color: #e2dccc;
+		text-transform: uppercase;
+		font-family: Syncopate;
+		font-weight: 700;
+		padding: 1rem;
+		cursor: pointer;
+		font-size: 1.6rem;
+		box-shadow: 0 0 0 6px #04110f;
+	}
+	.menu-download:hover {
+		background: #04110f;
+	}
+	@media (min-width: 550px) {
+		.menu-download {
+			padding: 2rem 4rem;
+		}
+	}
+</style>
