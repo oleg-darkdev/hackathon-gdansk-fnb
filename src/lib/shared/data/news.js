c6224a41c2f578ff007b4736d39b5ede8866fed6
DarkDev <darkdev_brand@riseup.net>
2024-04-09 16:18:45 +0300

    add news

--- src/lib/shared/data/news.js
+++ src/lib/shared/data/news.js
@@ -1,46 +1,74 @@
-
 const news = [
 	// {
 	// 	title: '',
 	// 	desc: ['', ''],
 	// 	img: '',
-	// 	date: ''
+	// 	date: '',
+	// 	id: 11
+	// },{
+	// 	title: '',
+	// 	desc: ['', ''],
+	// 	img: '',
+	// 	date: '',
+	// 	id: 10
+	// },{
+	// 	title: '',
+	// 	desc: ['', ''],
+	// 	img: '',
+	// 	date: '',
+	// 	id: 9
 	// },
 	// {
 	// 	title: '',
 	// 	desc: ['', ''],
 	// 	img: '',
-	// 	date: ''
+	// 	date: '',
+	// 	id: 8
 	// },
-	// {
-	// 	title: '',
-	// 	desc: ['', ''],
-	// 	img: '',
-	// 	date: ''
-	// },
-	// {
-	// 	title: '',
-	// 	desc: ['', ''],
-	// 	img: '',
-	// 	date: ''
-	// },
-	// {
-	// 	title: '',
-	// 	desc: ['', ''],
-	// 	img: '',
-	// 	date: ''
-	// },
-	// {
-	// 	title: '',
-	// 	desc: ['', ''],
-	// 	img: '',
-	// 	date: ''
-	// },
+	{
+		title: 'Przy hali można spróbować dzisiejszej zupki.',
+
+		desc: [
+			'Jakby ktoś się pospieszył to może jeszcze przy hali można spróbować dzisiejszej zupki. O ile oczywiście nikt jej nie zabrał',
+			''
+		],
+		img: '17.03.jpg',
+		date: '17.03',
+		id: 7
+	},
+	{
+		title: 'Zapraszamy na koncert  benefit',
+		desc: [
+			'Zapraszamy na koncert  benefit na naszą akcję rozdawania posiłków osobom potrzebującym. ',
+			''
+		],
+		img: '21.02.jpg',
+		date: '21.02',
+		id: 6
+	},
+	{
+		title: 'Młodzi na gotowaniu dekadę temu i wczoraj:',
+		desc: ['', ''],
+		img: '11.02.jpg',
+		date: '11.02',
+		id: 5
+	},
+	{
+		title: 'Jest dużo więcej przechodniów niż zazwyczaj.',
+		desc: [
+			'Co roku jest kilka takich niedziel, że na Długim Targu  jest dużo więcej przechodniów niż zazwyczaj.',
+			''
+		],
+		img: '28.01.jpg',
+		date: '28.01',
+		id: 4
+	},
 	{
 		title: 'Była zupa, ciasto i ubrania.',
 		desc: ['Jedzenie Zamiast Bomb, była zupa, ciasto i ubrania, zabrakło przesłania.', ''],
 		img: '21.01.jpg',
-		date: '21.01'
+		date: '21.01',
+		id: 3
 	},
 
 	{
@@ -51,7 +79,8 @@ const news = [
 		],
 		date: '14.01.2024',
 
-		img: '14.01.jpg'
+		img: '14.01.jpg',
+		id: 2
 	},
 	{
 		title: 'Niższe temperatury i od razu rekordową frekwencja.',
@@ -59,9 +88,9 @@ const news = [
 			'Niższe temperatury i od razu rekordową frekwencja. Rozdaliśmy zupę, kilkadziesiąt kotletów schabowych ale z soi, ciasto, resztkę cukierków z halloween, pasztet wegański'
 		],
 		date: '07.01.2024',
-		img: '07.01.jpg'
+		img: '07.01.jpg',
+		id: 1
 	}
 ];
-
 
 export default news;
