2c5e3a718289743a2e84a938826b854932475f2f
DarkDev <darkdev_brand@riseup.net>
2024-04-09 18:20:43 +0300

    fix margins on mobile & laptop

--- src/lib/entities/ui/landing/DarkDevCard.svelte
+++ src/lib/entities/ui/landing/DarkDevCard.svelte
@@ -1,8 +1,16 @@
-<a href="/" class="w-full">
+<a href="/" class="darkdev-card w-full">
 	<div
-		class="hover:delay-550 mt-10 w-full px-4 hover:-translate-y-1 hover:scale-105 hover:transition hover:duration-1000 hover:ease-in-out"
+		class="hover:delay-550 -mt-6 mb-4 w-full px-4 hover:-translate-y-1 hover:scale-105 hover:transition hover:duration-1000 hover:ease-in-out lg:-mb-32 xl:-mb-32"
 	>
 		<h3 class="text-primary text-center">Development by</h3>
 		<img src="./images/projects/darkdev.svg" alt="" />
 	</div>
 </a>
+
+<style>
+	@media (min-width: 1200px) {
+		/* .darkdev-card {
+
+		} */
+	}
+</style>
