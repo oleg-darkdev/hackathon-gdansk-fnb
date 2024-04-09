89b30d7e7612b7eb325fcec9f1c8a60fc01d1020
DarkDev <darkdev_brand@riseup.net>
2024-04-09 15:15:56 +0300

    stable v.

---
+++ src/lib/widgets/ui/landing/Footer.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { EmailLink, SocialLink } from '$shared';
+</script>
+
+
+<footer class="justiy-between flex flex-wrap items-center">
+	<a name="contact" />
+
+	<p class="mb-2 w-full">Dołącz do nas <br /> na kanałach społecznościowych!</p>
+	<div class="mx-auto flex w-full max-w-[230px] flex-row justify-between">
+		<SocialLink link="https://www.facebook.com/FNBgdansk/" image="facebook.svg" />
+		<EmailLink />
+	</div>
+</footer>
+
+<style>
+	footer {
+		margin-top: 1rem;
+		font-weight: 700;
+		text-transform: uppercase;
+		font-size: 1.3rem;
+	}
+
+	@media (min-width: 550px) {
+		footer {
+			font-size: 1.5rem;
+		}
+		footer p {
+			flex: 1;
+			margin: 0;
+		}
+	}
+</style>
