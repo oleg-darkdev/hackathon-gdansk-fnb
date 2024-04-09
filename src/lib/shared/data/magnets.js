53ccc49de59d79835fcd6d1cacb1085d10fb9906
DarkDev <darkdev_brand@riseup.net>
2024-04-09 15:16:49 +0300

    stable v.

---
+++ src/lib/shared/data/magnets.js
@@ -0,0 +1,44 @@
+const magnets = [
+	{
+		title: 'Drewniany magnes na lodówkę: Chaos Star',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '01.png',
+		code: '',
+		link: '/'
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: OI!',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '02.png',
+		code: '',
+		link: '/'
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: Girl Power',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '03.png',
+		code: '',
+		link: '/'
+	},
+	{
+		title: 'Drewniany magnes na lodówkę: Strong woman',
+		desc: 'Drewniany magnes na lodówkę',
+		price: '3',
+		img: '04.png',
+		code: '',
+		link: '/'
+	}
+	// {
+	// 	title: 'Lorem ipsum',
+	// 	desc: 'Drewniany magnes na lodówkę',
+	// 	price: '3',
+	// 	img: '05.png',
+	// 	code: '',
+	// 	link: '/'
+	// }
+];
+
+export default magnets;
---
+++ src/lib/shared/data/news.js
@@ -0,0 +1,67 @@
+
+const news = [
+	// {
+	// 	title: '',
+	// 	desc: ['', ''],
+	// 	img: '',
+	// 	date: ''
+	// },
+	// {
+	// 	title: '',
+	// 	desc: ['', ''],
+	// 	img: '',
+	// 	date: ''
+	// },
+	// {
+	// 	title: '',
+	// 	desc: ['', ''],
+	// 	img: '',
+	// 	date: ''
+	// },
+	// {
+	// 	title: '',
+	// 	desc: ['', ''],
+	// 	img: '',
+	// 	date: ''
+	// },
+	// {
+	// 	title: '',
+	// 	desc: ['', ''],
+	// 	img: '',
+	// 	date: ''
+	// },
+	// {
+	// 	title: '',
+	// 	desc: ['', ''],
+	// 	img: '',
+	// 	date: ''
+	// },
+	{
+		title: 'Była zupa, ciasto i ubrania.',
+		desc: ['Jedzenie Zamiast Bomb, była zupa, ciasto i ubrania, zabrakło przesłania.', ''],
+		img: '21.01.jpg',
+		date: '21.01'
+	},
+
+	{
+		title: 'Dziś znacznie cieplej niż przed tygodniem.',
+		desc: [
+			'Dziś znacznie cieplej niż przed tygodniem, więc i mniej potrzebujących się zjawiło po gorący posiłek. Ale nic się nie zmarnowało.',
+			''
+		],
+		date: '14.01.2024',
+
+		img: '14.01.jpg'
+	},
+	{
+		title: 'Niższe temperatury i od razu rekordową frekwencja.',
+		desc: [
+			'Niższe temperatury i od razu rekordową frekwencja. Rozdaliśmy zupę, kilkadziesiąt kotletów schabowych ale z soi, ciasto, resztkę cukierków z halloween, pasztet wegański'
+		],
+		date: '07.01.2024',
+		img: '07.01.jpg'
+	}
+];
+
+
+export default news;
