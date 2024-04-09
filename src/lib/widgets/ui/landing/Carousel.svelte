f97a3f2f59ea0224208f0389b5e003d4ca03c554
DarkDev <darkdev_brand@riseup.net>
2024-04-09 22:09:36 +0300

    fix for mobile

--- src/lib/widgets/ui/landing/Carousel.svelte
+++ src/lib/widgets/ui/landing/Carousel.svelte
@@ -13,8 +13,35 @@
 	<Carousel bullets={false} interval={1800}>
 		{#each data as text}
 			<CarouselItem style="">
-				<Hero style=" height:480px;" title={text} />
+				<div
+					style=""
+					class="slide-wrap hero hero-default hero-secondary border"
+				>
+					<div class="hero-body">
+						<h2 class="hero-title lg:text-5xl text-3xl md:text-4xl">{text}</h2>
+						<!-- <p class="hero-text-lead ">{text}</p> -->
+						<div />
+					</div>
+				</div>
 			</CarouselItem>
 		{/each}
 	</Carousel>
 </div>
+
+
+<style>
+  @media (min-width: 800px) {
+		.slide-wrap {
+      width: 450px;
+      height: 450px;
+      padding: 2rem 3rem;
+    }
+	}
+  @media (max-width: 800px) {
+		.slide-wrap {
+      width: 270px;
+      height: 260px;
+      padding: 0.6rem 1rem;
+    }
+	}
+</style>
