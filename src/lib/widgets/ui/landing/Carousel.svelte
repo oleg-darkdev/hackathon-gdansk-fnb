e814581e90446e6a391b7d0440ade7b9a6d3f7e8
DarkDev <darkdev_brand@riseup.net>
2024-04-09 15:18:21 +0300

    alfa v.

---
+++ src/lib/widgets/ui/landing/Carousel.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { i } from '@inlang/sdk-js';
+	// import {HeadHrefLangs} from '$shared';
+	import { Carousel, CarouselItem, Hero } from 'spaper';
+</script>
+
+<svelte:head>
+	<title>SvelteKit + TypeScript + TailwindCSS starter</title>
+	<!-- <HeadHrefLangs /> -->
+</svelte:head>
+<!-- `
+<h1 class="text-5xl font-extralight">{i('welcome')}</h1>
+<p>{@html i('description')}</p>` -->
+
+<section>
+	<Carousel bullets={false} interval={1400}>
+		<CarouselItem>
+			<Hero title="Hey there!" />
+		</CarouselItem>
+	</Carousel>
+</section>
