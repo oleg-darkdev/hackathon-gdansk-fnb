4ec2e8d0fb132c5be4657def5d628d3d1e07780a
DarkDev <darkdev_brand@riseup.net>
2024-04-10 17:05:38 +0300

    add more darkness color

--- src/lib/entities/ui/landing/DarkDevCard.svelte
+++ src/lib/entities/ui/landing/DarkDevCard.svelte
@@ -2,8 +2,8 @@
 	<div
 		class="hover:delay-550 -mt-6 mb-4 w-full px-4 hover:-translate-y-1 hover:scale-105 hover:transition hover:duration-1000 hover:ease-in-out lg:-mb-32 xl:-mb-32"
 	>
-		<h3 class="text-primary text-center">Development by</h3>
-		<img src="./images/projects/darkdev.svg" alt="" />
+		<h3 class=" text-center text-[#04110f]">Development by</h3>
+		<img style='border-color: #04110f !important;' src="./images/projects/darkdev.svg" alt="" />
 	</div>
 </a>
 
